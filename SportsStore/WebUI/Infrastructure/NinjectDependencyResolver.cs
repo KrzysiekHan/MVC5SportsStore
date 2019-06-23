34dd354ca069315f2e2030094e40c63ee752ad5b
Krzysztof Handzlik <krzysztof.handzlik@nemak.com>
2019-06-23 21:36:45 +0200

    połączenie z ninject, product repository, projects

---
+++ SportsStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //tutaj umieszczamy dodatkowe powiązania
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>{
+                new Product { Name = "Piłka nożna", Price = 25 },
+                new Product { Name = "Deska surfingowa", Price = 179 },
+                new Product { Name = "Buty do biegania", Price = 95 }
+                });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
