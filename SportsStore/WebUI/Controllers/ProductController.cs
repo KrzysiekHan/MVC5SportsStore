fc95c70c488df46a536bf604e62e0f240c20b350
Krzysztof Handzlik <krzysztof.handzlik@nemak.com>
2019-06-24 08:44:51 +0200

    stronicowanie

--- SportsStore/WebUI/Controllers/ProductController.cs
+++ SportsStore/WebUI/Controllers/ProductController.cs
@@ -11,14 +11,19 @@ namespace WebUI.Controllers
     {
 
         private IProductRepository repository;
+        private int PageSize;
+
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
 
     }
