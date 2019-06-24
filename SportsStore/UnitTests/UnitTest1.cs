7e20b8a6f1f12501b7ba5e3ef621442cc52e0de0
Krzysztof Handzlik <krzysztof.handzlik@nemak.com>
2019-06-24 14:37:45 +0200

    filter by category

--- SportsStore/UnitTests/UnitTest1.cs
+++ SportsStore/UnitTests/UnitTest1.cs
@@ -34,7 +34,7 @@ namespace UnitTests
             controller.PageSize = 3;
 
             //act
-           ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(null,2).Model;
 
             //assert
             Product[] prodArray = result.Products.ToArray();
@@ -83,7 +83,7 @@ namespace UnitTests
             controller.PageSize = 3;
 
             //act
-            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(null,2).Model;
 
             //assert 
             PaginInfo paginInfo = result.PaginInfo;
@@ -91,6 +91,32 @@ namespace UnitTests
             Assert.AreEqual(paginInfo.ItemsPerPage, 3);
             Assert.AreEqual(paginInfo.TotalItems, 5);
             Assert.AreEqual(paginInfo.TotalPages, 2);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product{ProductID = 1,Name = "P1",Category = "Cat1"},
+                new Product{ProductID = 2,Name = "P2",Category = "Cat2"},
+                new Product{ProductID = 3,Name = "P3",Category = "Cat1"},
+                new Product{ProductID = 4,Name = "P4",Category = "Cat2"},
+                new Product{ProductID = 5,Name = "P5",Category = "Cat3"}
+            }
+            );
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            Product[] result = ((ProductListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            //assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
--- SportsStore/WebUI/Models/ProductListViewModel.cs
+++ SportsStore/WebUI/Models/ProductListViewModel.cs
@@ -10,5 +10,6 @@ namespace WebUI.Models
     {
         public IEnumerable<Product> Products { get; set; }
         public PaginInfo PaginInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
