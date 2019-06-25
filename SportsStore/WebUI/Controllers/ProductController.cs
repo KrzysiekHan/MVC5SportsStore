469c0e5b54457a72c92954b9c07dbcde2afdc01f
Krzysztof Handzlik <krzysztof.handzlik@nemak.com>
2019-06-25 08:09:54 +0200

    selected + paging correction

--- SportsStore/WebUI/Controllers/ProductController.cs
+++ SportsStore/WebUI/Controllers/ProductController.cs
@@ -32,7 +32,7 @@ namespace WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count(),
+                    TotalItems = category == null?repository.Products.Count():repository.Products.Where(e=>e.Category == category).Count(),
                 },
                 CurrentCategory = category
             };
