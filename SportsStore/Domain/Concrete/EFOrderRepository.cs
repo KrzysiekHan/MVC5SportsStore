3dd2c728c23d831d5d16796bcad6e7eda943b2ba
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-07-28 00:12:45 +0200

    prevent order repository from inserting products

--- SportsStore/Domain/Concrete/EFOrderRepository.cs
+++ SportsStore/Domain/Concrete/EFOrderRepository.cs
@@ -2,6 +2,7 @@
 using Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,17 @@ namespace Domain.Concrete
         {
             if (order.Id == 0)
             {
-                context.OrderHeaders.Add(order);
+                //context.Set<School>().Add(newItem);
+                // use the following statement so that City won't be inserted
+                //context.Entry(newItem.City).State = EntityState.Unchanged;
+
+                //context.OrderHeaders.Add(order);
+                context.Set<OrderHeader>().Add(order);
+                foreach (var item in order.OrderDetail)
+                {
+                    context.Entry(item.Product).State = EntityState.Unchanged;
+                }
+                
             }
             context.SaveChanges();
         }
